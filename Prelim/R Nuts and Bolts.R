1356b4f060e18ad6ea5a515018bf53d4b55c7f6f
LouKris02 <loukrisp@gmail.com>
2018-07-10 16:08:16 +0800

    Activity

---
+++ Prelim/R Nuts and Bolts.R
@@ -0,0 +1,48 @@
+x <- 100
+x
+print(x)
+msg <- "hello"
+y <- 1:20
+y- 1.3
+x <- c(TRUE, 8.7)
+x
+a <- c(5,7)
+a
+b <- list("Lou", 1.3, 5+4i, TRUE)
+b
+c <- 3+4i
+d<- 5+6i
+e <- C+d
+e
+f <- c*d
+as.numeric(c)
+
+as.numeric(a)
+as.logical(a)
+a <- c(0,1,5)
+
+as.numeric(a)
+m<-matrix(nrow = 2, ncol = 3)
+m
+m <- 4:8
+dim(m)
+m<- 1:10
+dim(m) <- c(2,5)
+m
+attributes(m)
+dim(m)
+attributes(m)
+x <-1:5
+y<- 6:10
+cbind(x,y)
+rbind(x,y)
+
+m <- factor(c("blue","yellow","red","yellow", "blue", "blue"))
+m
+table(m)
+unclass(m)
+m <- factor(c("blue","yellow","red","yellow", "blue", "blue"))
+levels = c("yellow", "red", "blue")
+m
+table(m)
+class(m)
